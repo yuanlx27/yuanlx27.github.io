b5634732fe9182661fcb332532c75a9fb414a5b4
John Smith <johnsmith0x3f@gmail.com>
2025-06-14 10:14:48 +0000

    feat: update URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://yuanlx27.github.io",
-	// base: "/",
+	base: "/fuwari",
 	trailingSlash: "ignore",
 	integrations: [
 		tailwind({
